5106c6082a799d9a7aac8d39096afa3d6ec3d90e
jutamartfai <jutamart_a@cmu.ac.th>
2017-01-24 10:22:18 +0700

    24/01/60 edit tamp admin

--- WebsiteForStudyKanji/controllers/SiteController.php
+++ WebsiteForStudyKanji/controllers/SiteController.php
@@ -130,6 +130,19 @@ class SiteController extends Controller
     public function actionTemplate()
     {
         $this->layout = 'template';
+
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+
         return $this->render('template');
     }
 
